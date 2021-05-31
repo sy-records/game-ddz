8fb84a133db3abac2012c1cc7a3f40d4aad06711
sy-records <52o@qq52o.cn>
2021-05-31 10:28:48 +0800

    Update php_cs

--- config/autoload/game.php
+++ config/autoload/game.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * @link     https://www.hyperf.io
  * @document https://doc.hyperf.io
  * @contact  group@hyperf.io
- * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
     'user_info_key' => 'user:info:%s',     //用户信息redis的key，fd对应用户信息
